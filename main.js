df58e15d1eb9afdb1f8b97d38a9987228d0c82f7
Jahmil <roy_animo@hotmail.com>
2025-05-27 22:25:53 +0200

    adding tags to the DOM

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 let sideBar = document.querySelector(".side-bar");
+let sideBarUl = document.querySelector(".side-bar ul");
+
+let toggleSideBar = function() {
+    sideBar.style.width = "200px";
+    sideBarUl.style.visibility = "visible";
+}
+
