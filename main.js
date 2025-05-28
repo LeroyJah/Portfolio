34c1e3e21b34d9bb51edd998f900caa6549cb922
Jahmil <roy_animo@hotmail.com>
2025-05-28 23:30:34 +0200

    made button click function on boolean value

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 let sideBar = document.querySelector(".side-bar");
 let sideBarUl = document.querySelector(".side-bar ul");
 
-let toggleSideBar = function() {
-    sideBar.style.width = "200px";
-    sideBarUl.style.visibility = "visible";
+let sideBarStatus = false;
+
+let toggleSideBar = function(){
+    if(sideBarStatus === false){
+        sideBar.style.width = "200px";
+        sideBarUl.style.visibility = "visible";
+
+        sideBarStatus = true;
+    }
+
+    else if(sideBarStatus === true){
+        sideBar.style.width = "75px";
+        sideBarUl.style.visibility = "hidden";
+
+        sideBarStatus = false;
+    }
 }
+
+
 
