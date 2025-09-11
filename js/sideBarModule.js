242c9e0f9d86d7681d0d210a50c2ab3e8fa3031d
Jahmil <roy_animo@hotmail.com>
2025-09-11 22:43:51 +0200

    semi functional, but sideBarButton gives an error

--- js/sideBarModule.js
+++ js/sideBarModule.js
@@ -1,3 +1,9 @@
-function getSideBar(){
-    
+export function getSideBar(){
+    const sidenav = document.querySelector('.side-bar')
+
+    fetch('/sidebar.html')
+    .then(res=>res.text())
+    .then(data=>{
+        sidenav.innerHTML=data
+    })
 }
